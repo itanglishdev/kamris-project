6c7ccdc9a987495244fa5048fee64d17cd8f17eb
Chris <chrishethertonweb@gmail.com>
2022-11-10 13:36:51 +0100

    created productCard

---
+++ src/components/ProductCard.js
@@ -0,0 +1,18 @@
+
+
+
+function ProductCard(props) {
+
+
+
+    return ( 
+        <div className='productCard'>
+            <h2 className="product-card__name" >{props.name}</h2>
+            <img className="product-card__image" src={props.img} alt='' />
+            <p className="product-card__description" >{props.description}</p>
+            <p className="product-card__price" >{props.price}</p>
+        </div>
+     );
+}
+
+export default ProductCard;
